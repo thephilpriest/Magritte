07b8d60f3c7958a60a8f7059991152d397416866
thephilpriest <thephilpriest@gmail.com>
2024-12-21 16:01:55 -0500

    WIP

--- magritte.py
+++ magritte.py
@@ -6,16 +6,19 @@ class smidgeon:
 
     def __init__(self, params: dict):
         self.unq_id = uuid.uuid4()
-        self.length = params['length']
-        self.cur_velocity = params['intl_vel'] 
-        self.max_velocity = params['max_vel']
-        self.max_acceleration = params['max_acc]']
-        self.inst_acceleration = params['intl_acc']
-        self.acceleratiness = params['']
-        self.tailgatiness =
-        self.patience # more impatient smidgeons may close the gap as smidgeons several units ahead of them start advancing in anticipation of the smidgeon immediately in front of them advancing
-        self.erraticness = 
-        self.intention # is this just accelerate/decelerate or is it accomplish a certain goal? I feel like the goal is dynamic and changes iteration to iteration; that should be a function of the "iness" values and maybe be augmented by a value for eraticness (how rigidly does the smidgeon adhere to its "iness" values.
+        self.length = params['length'] # How long is this smidgeon object "bumper to bumper"
+        self.cur_velocity = params['intl_vel'] # Current rate of travel
+        self.max_velocity = params['max_vel'] # Maximum rate of travel
+        self.max_acceleration = params['max_acc'] # Maximum rate of acceleration
+        self.max_decceleration = params['max_dec'] # Maximum rate of deceleration
+        self.inst_celeration = params['intl_acc'] # Current rate of acceleration; positive for accelerating, negative for decelerating
+        self.acceleratiness = params[''] # Willingness to accelerate at the maximum rate
+        self.deceleratiness = params[''] # Willingness to decelerate at the maximum rate
+        self.tailgatiness = params [''] # Willingness to follow the smidgeon ahead of it closely
+        self.speederiness = params[''] # Willingness to travel at a rate that exceeds to limit for the pipe
+        self.patience = params[''] # more impatient smidgeons may close the gap as smidgeons several units ahead of them start advancing in anticipation of the smidgeon immediately in front of them advancing
+        self.erraticness = params[''] # How rigidly does this smidgeon adhere to the "iness" and patience parameters
+        self.intention = params[''] # is this just accelerate/decelerate or is it accomplish a certain goal? I feel like the goal is dynamic and changes iteration to iteration; that should be a function of the "iness" values and maybe be augmented by a value for eraticness (how rigidly does the smidgeon adhere to its "iness" values.
         
     
 #class pipe:
